c0bfe4565bc698598a79be05d44fbc0c15cc2853
Jesse Chung <kjesse.chung@samsung.com>
2018-12-11 18:18:33 +0900

    cleanup

--- dist/constants.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-exports.ACTION_DIRECT_UPDATE = Symbol('ACTION_DIRECT_UPDATE');
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-exports.directUpdateEnhancer = require('./directUpdateEnhancer');
-exports.directUpdate = require('./directUpdate');
